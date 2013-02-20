9ab1927a5112a2755b80826939ed3a3222a80189
Paul Jolly <paul@myitcv.org.uk>
2013-02-19 23:05:16 -0800

    Initial version of complete example

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-OmniauthGoogleOauth2Example::Application.config.secret_token = 'ad3bf5c64e825c6fa22e75eee42d97fb86a03cd5a43d64c81ab2097336b820ac30f37206d20e6b22127db0b7a34ea74411ab9e5f2eaf518f70cb424a1b788c18'
+OmniauthGoogleOauth2Example::Application.config.secret_token = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
