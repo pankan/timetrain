3ee8405dec3a11ac6a12dad8eb716dda88f16d09
Pankan <kiran@qburst.com>
2014-06-24 16:55:16 +0530

    Phase 1

--- config/initializers/oauth.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, 
-    ENV['OAUTH_CLIENT_ID'],
-    ENV['OAUTH_CLIENT_SECRET'],
-    {name: "google_login", approval_prompt: ''}
-end
--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-OmniauthGoogleOauth2Example::Application.routes.draw do
-  get "home/index"
-  root :to => 'home#index'
-  match "/auth/google_login/callback" => "sessions#create"
-  match "/signout" => "sessions#destroy", :as => :signout
-end
